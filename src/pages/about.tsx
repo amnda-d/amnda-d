0828d7030059b4c823dfe8b80a6f42a8a9c977de
Amanda Doucette <amanda@amnda.me>
2022-06-16 13:21:15 -0400

    update cv and presentations

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,7 @@ export const About = (props: { colors: Object }) => (
     <Bio colors={props.colors}>
       <Title colors={props.colors}>About Amanda</Title>
       <div>
-        I'm a former software engineer and current PhD student in linguistics at{' '}
+        I am a PhD student in linguistics at{' '}
         <a
           className="reverse"
           href="https://www.mcgill.ca/linguistics/"
@@ -16,37 +16,71 @@ export const About = (props: { colors: Object }) => (
         >
           McGill University
         </a>{' '}
-        interested in how machine learning - mainly artificial neural networks -
-        can be used to model language learning and linguistic representation in
-        the human brain. Specifically, I'm interested in how humans learn
-        phonological patterns, and how phonological features are represented in
-        the brain. My name is pronounced [əˈmændə duˈsɛt] and my pronouns are
+        , and a former software engineer. I'm interested in how humans learn
+        phonological patterns, and how these patterns are represented in the
+        brain. My name is pronounced [əˈmændə duˈsɛt] and my pronouns are
         they/them.
       </div>
       <Subtitle colors={props.colors}>Publication</Subtitle>
       <div>
-        <b>2017</b>.{' '}
         <a
           className="reverse"
           href="https://www.aclweb.org/anthology/W17-0705/"
           target="_blank"
         >
-          Inherent Biases of Recurrent Neural Networks for Phonological
-          Assimilation and Dissimilation.
-        </a>{' '}
-        Proceedings of the 7th Workshop on Cognitive Modeling and Computational
-        Linguistics (CMCL 2017), 35–40.
+          <b>
+            Inherent Biases of Recurrent Neural Networks for Phonological
+            Assimilation and Dissimilation
+          </b>
+          .
+        </a>
+        <br />
+        Amanda Doucette.
+        <br />
+        <i>
+          Proceedings of the 7th Workshop on Cognitive Modeling and
+          Computational Linguistics
+        </i>
+        , 35–40, <b>2017</b>.
       </div>
-      <Subtitle colors={props.colors}>Presentation</Subtitle>
+      <Subtitle colors={props.colors}>Presentations</Subtitle>
       <div>
-        <b>2019</b>.{' '}
+        <a
+          className="reverse"
+          href="ADoucette_LabPhon_Presentation_v2.pdf"
+          target="_blank"
+        >
+          <b>
+            Identity, similarity, and the OCP: A model of co-occurrence in 107
+            languages
+          </b>
+          .
+        </a>
+        <br />
+        Amanda Doucette, Morgan Sonderegger, Timothy J. O'Donnell, and Heather
+        Goad.
+        <br />
+        <i>LabPhon 18.</i> <b>June 25, 2020</b>.
+        <a
+          className="reverse"
+          href="https://github.com/amnda-d/co-ocurrence-harmony"
+          target="_blank"
+        >
+          {' '}
+          [code]
+        </a>
+      </div>
+      <div>
         <a className="reverse" href="2019_ML_and_AI.pdf" target="_blank">
-          Introduction to AI and Machine Learning.
-        </a>{' '}
-        General audience talk on the basics of AI and related ethical issues.
-        Given at Soho House Ludlow Tech Club, New York, NY (May 14) and for
-        visiting students from Osaka College of Design and IT, Los Angeles, CA
-        (November 15).
+          <b>Introduction to AI and Machine Learning</b>.
+        </a>
+        <br />
+        Amanda Doucette.
+        <br />
+        <i>Soho House Ludlow Tech Club</i>. New York, NY. <b>May 14, 2019</b>.
+        <br />
+        <i>Originate</i> (for visiting students from Osaka College of Design and
+        IT). Los Angeles, CA. <b>November 15, 2019</b>
       </div>
     </Bio>
   </Page>
